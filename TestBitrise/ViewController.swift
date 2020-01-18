b9f0784ce167abc7d304fd442257725d9870bae6
Petr Novoselov <pnovoselov@swiftcom.uk>
2020-01-18 18:42:57 +0300

    Initial commit

---
+++ TestBitrise/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestBitrise
+//
+//  Created by Petr on 18.01.2020.
+//  Copyright © 2020 Higher School of ITIS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
